1ea376f3053168935e920f8bef4147b0ec2879f7
Ládson Sá <123712890+PreGUU@users.noreply.github.com>
2026-07-29 20:01:22 -0300

    feat: add loading indicator component

---
+++ src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -0,0 +1,11 @@
+import styles from "./LoadingIndicator.module.css";
+
+export function LoadingIndicator() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+        </div>
+    );
+}
